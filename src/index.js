42c7b8f270daf65b555d548dea48c11d9281a4d3
Raymond Zeng <shilei.zeng98@outlook.com>
2023-08-24 17:04:29 -0400

    Generated HTML footer through js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import initializeWebsite from "./website";
-import githubIcon from './images/github_icon.png'
+
 
 initializeWebsite();
-
-function addGithubIcon(){
-  const icon = new Image();
-  icon.src = githubIcon;
-  icon.alt = 'github_icon'
-  icon.id = 'github-icon'
-  
-  document.getElementById("github_link").appendChild(icon);
-}
-
-addGithubIcon();
 
 console.log("Index.js is working")
