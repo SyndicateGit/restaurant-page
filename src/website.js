c3377c6a71e9499ddc5774035091656d7a18c354
Raymond Zeng <shilei.zeng98@outlook.com>
2023-08-24 17:04:06 -0400

    Generate html footer via js

--- src/website.js
+++ src/website.js
@@ -1,11 +1,58 @@
+//Generating main content from tabs
 import loadHome from "./home";
 import loadMenu from "./menu";
 import loadContact from "./contact";
 
+// Github icon for footer
+import githubIcon from './images/github_icon.png'
+
+// HEADER SECTION
+
+function addHeader(){
+  
+}
+
+// FOOTER SECTION
+function createGithubIcon(){
+  const icon = new Image();
+  icon.src = githubIcon;
+  icon.alt = 'github_icon'
+  icon.id = 'github-icon'
+  return icon;
+}
+
+function createFooter(){
+  // Footer container
+  const footer = document.createElement('div');
+  footer.className = 'footer';
+  
+  const text = document.createElement('p');
+  text.textContent = 'Copyright © '
+
+  // Clickable link to github account: SyndicateGit + (icon)
+  const githubLink = document.createElement('a')
+  githubLink.id = 'github_link'
+  githubLink.href = "https://github.com/SyndicateGit"
+
+  const githubAccount = document.createElement('p');
+  githubAccount.textContent = 'SyndicateGit'
+
+  githubLink.appendChild(githubAccount);
+  githubIcon = createGithubIcon();
+  githubLink.appendChild(githubIcon);
+
+  // Add text and link to footer
+  footer.appendChild(text);
+  footer.appendChild(githubLink);
+
+  return footer
+}
+
+
+
+// Main Function to export
 function initializeWebsite(){
-  console.log("initializeWebsite is working")
-  loadMenu();
-  loadHome();
-  loadContact();
+  const content = document.getElementById("content")
+  content.appendChild(createFooter());
 }
 export default initializeWebsite;
